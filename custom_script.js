b333a80d49044821f98040b61d19d5ac48e442b0
Spencer McMahon <spencermcmahon@MacBook-Pro-10.local>
2020-11-10 11:34:10 -0500

    repeat button plus new song

--- custom_script.js
+++ custom_script.js
@@ -27,36 +27,41 @@ const song = document.querySelector('#song'); // audio object
 const songTitle = document.querySelector('.song-title'); // element where track title appears
 const progressBar = document.querySelector('#progress-bar'); // element where progress bar appears
 let pPause = document.querySelector('#play-pause'); // element where play and pause image appears
+let loopButton = document.querySelector('#repeat'); // element where play and pause image appears
 
-songIndex = 1;
-songs = ['./assets/audio/Under_the_sea_lofi.mp3', './assets/audio/the_lucky_one.mp3', './assets/audio/FEELINGIT.mp3', './assets/audio/GetOverIt.mp3', './assets/audio/SoulfulStrutFutureFunk.mp3', './assets/audio/M8sW8s808s.mp3', './assets/audio/scandalous.mp3', './assets/audio/JustKilledAnOldFriendButMakeItLofiHipHop.mp3', './assets/audio/finalproject.mp3', './assets/audio/CarFreestyle.mp3', './assets/audio/VIBE_CA$H.mp3', './assets/audio/yoshi.mp3', './assets/audio/Squoad.mp3', './assets/audio/its_you.mp3', './assets/audio/heat_lightning.mp3']
+songIndex = 0;
+songs = ['./assets/audio/Stay.mp3', './assets/audio/Under_the_sea_lofi.mp3', './assets/audio/the_lucky_one.mp3', './assets/audio/FEELINGIT.mp3', './assets/audio/GetOverIt.mp3', './assets/audio/SoulfulStrutFutureFunk.mp3', './assets/audio/M8sW8s808s.mp3', './assets/audio/scandalous.mp3', './assets/audio/JustKilledAnOldFriendButMakeItLofiHipHop.mp3', './assets/audio/finalproject.mp3', './assets/audio/CarFreestyle.mp3', './assets/audio/VIBE_CA$H.mp3', './assets/audio/yoshi.mp3', './assets/audio/Squoad.mp3', './assets/audio/its_you.mp3', './assets/audio/heat_lightning.mp3']
 // songTitles = ["Final Project", "just_killed_an_old_friend_but_make_it_lofi_hip_hop", "M8s, W8s, 808s", "The Lucky One", "Soulful Strut // Future Funk", "Squad", "VIBE CA$H", "Yoshi", "Car Freestyle (Instrumental)", "Get Over It", "Heat Lightning (Instrumental)", "It's You", "Scandalous (with LoloPop13)"]; // object storing track titles
-songTitles = ["Under the Sea (lofi cover)", "The Lucky One (prod. Nick Froelich)", "F E E L I N G I T ! !", "Get Over It", "Soulful Strut // Future Funk", "M8s, W8s, 808s (prod. Stu)", "Scandalous (with LoloPop13)", "just_killed_an_old_friend_but_make_it_lofi_hip_hop", "Final Project", "Car Freestyle (Instrumental)", "VIBE CA$H", "Yoshi", "Squad", "It's You", "Heat Lightning (Instrumental)"]
+songTitles = ["Stay", "Under the Sea (lofi cover)", "The Lucky One (prod. Nick Froelich)", "F E E L I N G I T ! !", "Get Over It", "Soulful Strut // Future Funk", "M8s, W8s, 808s (prod. Stu)", "Scandalous (with LoloPop13)", "just_killed_an_old_friend_but_make_it_lofi_hip_hop", "Final Project", "Car Freestyle (Instrumental)", "VIBE CA$H", "Yoshi", "Squad", "It's You", "Heat Lightning (Instrumental)"]
 // function where pp (play-pause) element changes based on playing boolean value - if play button clicked, change pp.src to pause button and call song.play() and vice versa.
-let playing = true;
+let loop = false;
+let playing = false;
 function playPause() {
-    if (playing) {
+    if (!playing) {
         const song = document.querySelector('#song'),
         thumbnail = document.querySelector('#thumbnail');
 
         pPause.src = "./assets/icons/pauseWhite.png"
 
         song.play();
-        playing = false;
+        playing = true;
     } else {
         pPause.src = "./assets/icons/playWhite.png"
 
         song.pause();
-        playing = true;
+        playing = false;
     }
+    console.log("Playing:", playing);
 }
 
 // automatically play the next song at the end of the audio object's duration
 song.addEventListener('ended', function(){
-    nextSong();
+    if (loop) previousSong();
+    else nextSong();
+    playing = false;
+    playPause();
 });
 
-// function where songIndex is incremented, song/thumbnail image/background image/song artist/song title changes to next index value, and playPause() runs to play next track
 function nextSong() {
     songIndex++;
     if (songIndex >= songs.length) {
@@ -65,11 +70,10 @@ function nextSong() {
     song.src = songs[songIndex];
     songTitle.innerHTML = songTitles[songIndex];
 
-    playing = true;
+    playing = false;
     playPause();
 }
 
-// function where songIndex is decremented, song/thumbnail image/background image/song artist/song title changes to previous index value, and playPause() runs to play previous track
 function previousSong() {
   if (song.currentTime > 2) {
     song.currentTime = 0;
@@ -83,9 +87,23 @@ function previousSong() {
     song.src = songs[songIndex];
     songTitle.innerHTML = songTitles[songIndex];
 
-    playing = true;
+    playing = false;
     playPause();
   }
+}
+
+function toggleLoop() {
+  if (loop) {
+    loop = false;
+    loopButton.style.backgroundColor = "#343a40";
+    loopButton.style.color = "#f7f7f7";
+  }
+  else {
+     loop = true;
+     loopButton.style.backgroundColor = "#f7f7f7";
+     loopButton.style.color = "#343a40";
+  }
+  console.log("loop:", loop)
 }
 
 // update progressBar.max to song object's duration, same for progressBar.value, update currentTime/duration DOM
